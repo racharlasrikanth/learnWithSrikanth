32edba29b475b289121151d6e1d4f8d002ec5746
racharlasrikanth <racharlasrikanth3211@gmail.com>
2020-07-29 13:56:29 +0530

    second commit

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -6,7 +6,9 @@ class Login extends React.Component {
     return (
       <div>
         <h1>This is Login form</h1>
-        <form></form>
+        <form>
+          <input type="text" />
+        </form>
       </div>
     );
   }
